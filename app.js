8f3cf1c0e7b29697df9e34a03ab0320b9d751c1b
Chris Betz <scrapdog@gmail.com>
2012-06-11 10:09:58 -0400

    express framework; content pages

---
+++ app.js
@@ -0,0 +1,32 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http');
+
+var app = express();
+
+app.configure(function(){
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function (req, res) {
+    fs.readFile(__dirname + '/public/index.html', 'utf8', function (err, text) {
+        res.send(text);
+    });
+});
+
+http.createServer(app).listen(80);
+
+console.log("Express server listening on port 80");
