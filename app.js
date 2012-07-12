2c543afba928de232b455ba3fb7bba6a0e603d51
Chris Betz <scrapdog@gmail.com>
2012-07-12 13:36:18 -0400

    content changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
