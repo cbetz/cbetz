7bc51e586ccd9a13db3bf53d1b1ed0361edf72ed
Chris Betz <scrapdog@gmail.com>
2012-10-22 14:53:41 -0400

    turned on compression

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = express();
 app.configure(function(){
   app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(express.logger('dev'));
+  app.use(express.compress());
   app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
