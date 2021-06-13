27a2af0cb7adf0e2dec851baf5b590940f5deed9
Christopher Betz <scrapdog@gmail.com>
2021-06-12 21:58:27 -0400

    posts -> blog for legacy links

--- components/cover-image.js
+++ components/cover-image.js
@@ -14,7 +14,7 @@ export default function CoverImage({ title, url, slug }) {
   return (
     <div className="sm:mx-0">
       {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <Link as={`/blog/${slug}`} href="/blog/[slug]">
           <a aria-label={title}>{image}</a>
         </Link>
       ) : (
--- components/post-preview.js
+++ components/post-preview.js
@@ -17,7 +17,7 @@ export default function PostPreview({
         <CoverImage title={title} slug={slug} url={coverImage.url} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <Link as={`/blog/${slug}`} href="/blog/[slug]">
           <a className="hover:underline">{title}</a>
         </Link>
       </h3>
