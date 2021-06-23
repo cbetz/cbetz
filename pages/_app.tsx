6794593f158f67fc601c0675bd9f59dff39b63b0
Christopher Betz <scrapdog@gmail.com>
2021-06-23 15:49:01 -0400

    adding Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import "../styles/index.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-20420622-1"
+        strategy="afterInteractive"
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-20420622-1');`,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
