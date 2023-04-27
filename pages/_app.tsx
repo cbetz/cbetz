1893d0488d23c40ae7649fc120d544ed706599e1
Christopher Betz <458549+cbetz@users.noreply.github.com>
2023-04-26 22:32:34 -0400

    adding vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import "../styles/index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
